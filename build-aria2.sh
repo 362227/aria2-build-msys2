b5248a22289dce8e20a8ebaf2077fb7b3d93af46
362227 <91358231+362227@users.noreply.github.com>
2023-03-20 12:29:29 +0800

    Update build-aria2.sh

--- build-aria2.sh
+++ build-aria2.sh
@@ -150,26 +150,6 @@ git am -3 ../aria2-*.patch
 autoreconf -fi || autoreconf -fiv
 
 ./configure \
-    --host=$HOST \
-    --prefix=$PREFIX \
-    --without-included-gettext \
-    --disable-nls \
-    --with-libcares \
-    --without-gnutls \
-    --without-openssl \
-    --with-sqlite3 \
-    --without-libxml2 \
-    --with-libexpat \
-    --with-libz \
-    --with-libgmp \
-    --without-libssh2 \
-    --without-libgcrypt \
-    --without-libnettle \
-    --with-cppunit-prefix=$PREFIX \
-    ARIA2_STATIC=yes \
-    CPPFLAGS="-I$PREFIX/include" \
-    LDFLAGS="-L$PREFIX/lib -Wl,--gc-sections,--build-id=none" \
-    PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
 
 make 
 strip -s src/aria2c.exe
